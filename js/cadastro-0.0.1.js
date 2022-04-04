fd0029ad6c0a72e67ae9287c9401c29b3ac834a4
JoaoBembe <joao.bembe@discente.ufopa.edu.br>
2022-04-03 21:16:43 -0300

    reforma total do codigo js

--- js/cadastro-0.0.1.js
+++
@@ -1,8 +0,0 @@
-var Inputmask = require('inputmask');
-
-
-var selector = document.getElementById("fnumero");
-
-var im = new Inputmask("99-9999999");
-im.mask(selector);
-alert("teste")
--- js/cadastro.js
+++ js/cadastro.js
@@ -1,83 +1,57 @@
 const fields = document.querySelectorAll('[required]')
 
-function validateField(field) {
-  function verifyErrors() {
-    let errorFound = false
-    for (let error in field.validity) {
-      if (field.validity[error] && !field.validity.valid) {
-        errorFound = error
-      }
-    }
-    return errorFound
+for( field of fields ){
+  field.addEventListener("invalid", event => { 
+      // eliminar o bubble
+      event.preventDefault() 
+  })
+}
+
+function setErroNome() {
+  const spanError = fields[0].parentNode.querySelector('span.error')
+  if (fields[0].validity.valueMissing) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'Preenchimento obrigatório!'
+  } else {
+    spanError.classList.remove('active')
+    spanError.innerHTML = ''
   }
+}
 
-  function customMessage(typeError) {
-    const messages = {
-      text: {
-        valueMissing: 'Campo obrigatório'
-      },
-      email: {
-        valueMissing: 'Campo obrigatório',
-        typeMismatch: 'E-mail inválido'
-      },
-      number: {
-        valueMissing: 'Campo obrigatório'
-      }
-    }
-
-    return messages[field.type][typeError]
+function setErroEmail() {
+  const spanError = fields[1].parentNode.querySelector('span.error')
+  if (fields[1].validity.valueMissing) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'Preenchimento obrigatório!'
+  } else if (!fields[1].validity.valid) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'E-mail inválido!'
+  } else {
+    spanError.classList.remove('active')
+    spanError.innerHTML = ''
   }
+}
 
-  function setCustomMessage(message) {
-    const spanError = field.parentNode.querySelector('span.error')
-    if (message) {
-      spanError.classList.add('active')
-      spanError.innerHTML = message
-    } else {
+function setErrorCPFOnKeyUp() {
+  const spanError = fields[2].parentNode.querySelector('span.error')
+  if (fields[2].value.length == 14) {
+    if (validateCPF(removeCaracteres(fields[2].value))) {
       spanError.classList.remove('active')
       spanError.innerHTML = ''
-    }
-  }
-
-  return function () {
-    const error = verifyErrors()
-
-    if (error) {
-      const message = customMessage(error)
-      setCustomMessage(message)
     } else {
-      setCustomMessage()
-    }
-
-    //telefone
-    if (fields[3].value.length < 9 && fields[3].value.length > 0) {
-      const spanError = fields[3].parentNode.querySelector('span.error')
       spanError.classList.add('active')
-      spanError.innerHTML = 'Numero incompleto'
+      spanError.innerHTML = 'CPF invalido'
     }
-
-    //cpf
-  
   }
 }
 
-function customValidation(event) {
-  const field = event.target
-  const validation = validateField(field)
-  validation()
+function setErrorCPFOnBlur() {
+  const spanError = fields[2].parentNode.querySelector('span.error')
+  if (fields[2].value.length < 14) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'CPF incompleto!'
+  }
 }
-
-for (field of fields) {
-  field.addEventListener('invalid', event => {
-    event.preventDefault()
-    customValidation(event)
-  })
-  field.addEventListener('blur', customValidation)
-}
-
-document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault()
-})
 
 function mascaraCPF(i) {
   var v = i.value
@@ -93,34 +67,24 @@ function mascaraCPF(i) {
   if (v.length == 11) i.value += '-'
 }
 
-function mascaraTelefone(telefone) {
-  const textoAtual = telefone.value
-  const isCelular = textoAtual.length == 11
-  let textAjustado
-  if (isCelular) {
-    const parte1 = textoAtual.slice(0, 2)
-    const parte2 = textoAtual.slice(2, 3)
-    const parte3 = textoAtual.slice(3, 7)
-    const parte4 = textoAtual.slice(7, 11)
-    textAjustado = `${parte1} ${parte2} ${parte3}-${parte4}`
-    telefone.value = textAjustado
-  } else if (textoAtual.length == 10) {
-    const parte1 = textoAtual.slice(0, 2)
-    const parte2 = textoAtual.slice(2, 6)
-    const parte3 = textoAtual.slice(6, 10)
-    textAjustado = `(${parte1}) ${parte2}-${parte3}`
-    telefone.value = textAjustado
+function validarGitUrl() {
+  const spanError = fields[4].parentNode.querySelector('span.error')
+  if (fields[4].validity.valueMissing) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'Preenchimento obrigatório!'
+  } else if (fields[4].validity.typeMismatch) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'URL invalida!'
+  } else if (fields[4].validity.patternMismatch) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'Não é uma url do GitHub'
   } else {
+    spanError.classList.remove('active')
+    spanError.innerHTML = ''
   }
 }
 
-function removeCaracteres(str) {
-  const textAjustado = str.replace(/[^\w\s]/gi, '')
-  return textAjustado
-}
-
 function setErrorTelefone() {
-  
   const spanError = fields[3].parentNode.querySelector('span.error')
   if (fields[3].value.length == 11 || fields[3].value.length == 10) {
     if (fields[3].value.length < 9) {
@@ -130,10 +94,45 @@ function setErrorTelefone() {
       spanError.classList.remove('active')
       spanError.innerHTML = ''
     }
+  } else if (fields[3].value.length < 11 && fields[3].value.length > 0) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'Telefone incompleto!'
+  } else if (fields[3].value.length == 0) {
+    spanError.classList.add('active')
+    spanError.innerHTML = 'Preenchimento obrigatório!'
   } else {
     spanError.classList.remove('active')
     spanError.innerHTML = ''
   }
+}
+
+function mascaraTelefone(i) {
+  var v = i.value
+  var isMovel = false
+  var a = Array.from(i.value)
+
+  i.setAttribute('maxlength', '16')
+  if (v.length == 1) i.value = '(' + i.value
+  if (v.length == 3) i.value += ')'
+  if (v.length == 3) i.value += ' '
+
+  if (a[5] == 9) {
+    isMovel = true
+  } else {
+    isMovel = false
+  }
+  if (isMovel) {
+    if (v.length == 6) i.value += ' '
+    if (v.length == 11) i.value += '-'
+  }
+  if (!isMovel) {
+    if (v.length == 9) i.value += '-'
+  }
+}
+
+function removeCaracteres(str) {
+  const textAjustado = str.replace(/[^\w\s]/gi, '')
+  return textAjustado
 }
 
 function validateCPF(strCPF) {
@@ -141,6 +140,8 @@ function validateCPF(strCPF) {
   var Resto
   Soma = 0
   if (strCPF == '00000000000') return false
+
+  if (strCPF < 11) return false
 
   for (i = 1; i <= 9; i++)
     Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i)
@@ -157,20 +158,4 @@ function validateCPF(strCPF) {
   if (Resto == 10 || Resto == 11) Resto = 0
   if (Resto != parseInt(strCPF.substring(10, 11))) return false
   return true
-}
-
-function setErrorCPF() {
-  const spanError = fields[2].parentNode.querySelector('span.error')
-  if (fields[2].value.length == 14) {
-    if (validateCPF(removeCaracteres(fields[2].value))) {
-      spanError.classList.remove('active')
-      spanError.innerHTML = ''
-    } else {
-      spanError.classList.add('active')
-      spanError.innerHTML = 'CPF invalido'
-    }
-  } else {
-    spanError.classList.remove('active')
-    spanError.innerHTML = ''
-  }
 }
